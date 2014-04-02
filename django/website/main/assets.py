eb87b857ca4dd358fc2e9cc4644d84df4226b25a
Sarah Bird <sarah@bonvaya.com>
2014-04-02 13:21:48 -0700

    Add some styling and a dropdown explanation

--- django/website/main/assets.py
+++ django/website/main/assets.py
@@ -1,9 +1,21 @@
 from django_assets import Bundle, register
+
+# JAVASCRIPT
+
 js = Bundle('bower_components/jquery/dist/jquery.js',
+            'bower_components/bootstrap/js/collapse.js',
             filters='rjsmin',
             output='js/site.js')
-css = Bundle('bower_components/bootstrap/dist/css/bootstrap.css',
-             filters='cssmin',
+register('js_all', js)
+
+
+# CSS
+standard_scss = Bundle('scss/standard.scss',
+                       filters='pyscss',
+                       output='css/standard.css',
+                       depends='scss/*.scss')
+
+css = Bundle('bower_components/bootstrap/dist/css/bootstrap.min.css',
+             standard_scss,
              output='css/site.css')
 register('css_all', css)
-register('js_all', js)
