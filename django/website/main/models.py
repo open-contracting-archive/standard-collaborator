42a2ddf3f07ca6eba76f26843419d29a8cdc8789
Sarah Bird <sarah@bonvaya.com>
2014-06-25 06:14:09 -0700

    Add model repr

--- django/website/main/models.py
+++ django/website/main/models.py
@@ -5,9 +5,15 @@ class LatestVersion(models.Model):
     # This should only ever be an actual tag
     tag_name = models.CharField(max_length=30)
 
+    def __unicode__(self):
+        return u'%s' % self.tag_name
+
 
 class CachedStandard(models.Model):
     # This could be a sha
     tag_name = models.CharField(max_length=100)
     standard = models.TextField(blank=True)
     vocabulary = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return u'%s' % self.tag_name
