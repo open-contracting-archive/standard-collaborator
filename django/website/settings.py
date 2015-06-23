7bf79314100b76bf7311df1a3393171387a55a54
Ben Webb <bjwebb67@googlemail.com>
2015-06-23 09:48:05 +0100

    Add raven (so we can use getsentry.com for error reports)

--- django/website/settings.py
+++ django/website/settings.py
@@ -21,6 +21,7 @@ TEMPLATE_DEBUG = DEBUG
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 import private_settings
 SECRET_KEY = private_settings.SECRET_KEY
+RAVEN_CONFIG = private_settings.RAVEN_CONFIG
 ########## END SECRET CONFIGURATION
 
 
@@ -127,6 +128,7 @@ THIRD_PARTY_APPS = (
     'django_assets',
     'registration',
     'bootstrapform',
+    'raven.contrib.django.raven_compat',
 )
 
 # Apps specific for this project go here.
@@ -216,29 +218,32 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+#
+# (This is commented out, as we're now using Raven/Sentry instead.
+#
+#LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': False,
+#    'filters': {
+#        'require_debug_false': {
+#            '()': 'django.utils.log.RequireDebugFalse'
+#        }
+#    },
+#    'handlers': {
+#        'mail_admins': {
+#            'level': 'ERROR',
+#            'filters': ['require_debug_false'],
+#            'class': 'django.utils.log.AdminEmailHandler'
+#        }
+#    },
+#    'loggers': {
+#        'django.request': {
+#            'handlers': ['mail_admins'],
+#            'level': 'ERROR',
+#            'propagate': True,
+#        },
+#    }
+#}
 ########## END LOGGING CONFIGURATION
 
 
